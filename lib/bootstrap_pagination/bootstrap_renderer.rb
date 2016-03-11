efbeb6e5a86e6f96ed8a823b7dccec1530e99129
Bernard Banta <banta.bernard@gmail.com>
2016-03-11 13:29:49 +0300

    Changed the template

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -28,7 +28,7 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page == current_page
-        tag("li", tag("span", page), class: "active")
+        tag("li", link(page, "#"), class: "active")
       else
         tag("li", link(page, page, link_options.merge(rel: rel_value(page))))
       end
@@ -40,7 +40,7 @@ module BootstrapPagination
       if page
         tag("li", link(text, page, link_options), class: classname)
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", link(text, "#"), class: "%s disabled" % classname)
       end
     end
 
@@ -50,16 +50,16 @@ module BootstrapPagination
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "prev")
+      previous_or_next_page(num, "&laquo;", "prev")
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], "next")
+      previous_or_next_page(num, "&raquo;", "next")
     end
 
     def ul_class
-      ["pagination", @options[:class]].compact.join(" ")
+      "pagination"
     end
   end
 end
